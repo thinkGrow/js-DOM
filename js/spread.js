ff97697baded525cf79e87f8c0b8c12509af90cf
MaishaTahsin <rubaitreshad@gmail.com>
2025-05-24 21:56:13 +0600

    spread operator

---
+++ js/spread.js
@@ -0,0 +1,11 @@
+const friends = ["wade", "akhund", "zaman"]
+
+const newArray = ["hero alam", ...friends]
+
+console.log(newArray);
+
+
+const numbers = [1,5,6,7,129,502]
+
+console.log(Math.max(...numbers))
+
