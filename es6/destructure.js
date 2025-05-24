ab9ab8f5ee4b3a395725bc5af22581be36694185
MaishaTahsin <rubaitreshad@gmail.com>
2025-05-24 22:11:13 +0600

    destructure basics

---
+++ es6/destructure.js
@@ -0,0 +1,20 @@
+// const hena = {
+//     name : 'hena',
+//     age : 10,
+//     friends: ["hero alam", "bappa"],
+//     status: "not found"
+// }
+
+// console.log(hena.status)
+
+// destructure for obj
+// const {name, age, friends, status} = hena;
+
+
+const person = ["hero alam", {name:"gias"}, "hena", "bapparaj"]
+
+
+// destructure for
+const [test, obj, name, sadNayok] = person;
+
+console.log(sadNayok)
